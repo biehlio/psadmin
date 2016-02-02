8866f2097e92e41e8c3214f09aa170cee9e5f7ca
Matt Biehl <biehl.matt@gmail.com>
2016-02-01 20:39:17 -0800

    Finished: Redirect Demo

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
   <Route name='app' path='/' handler={require('./components/app.jsx')}>
@@ -12,6 +13,8 @@ var routes = (
     <Route name='authors' handler={require('./components/authors/authorPage.jsx')} />
     <Route name='about' handler={require('./components/about/aboutPage.jsx')} />
     <NotFoundRoute handler={require('./components/notFoundPage.jsx')} />
+    <Redirect from="about-us" to="about" />
+    <Redirect from="about/*" to="about" />
   </Route>
 );
 
