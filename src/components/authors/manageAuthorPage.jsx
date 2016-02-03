a445ea7ed8b722868f88fa02d30dec91c9a3c52c
Matt Biehl <mbiehl@frontporch.com>
2016-02-03 14:53:17 -0800

    Everything is working pre Flux

--- src/components/authors/manageAuthorPage.jsx
+++ src/components/authors/manageAuthorPage.jsx
@@ -25,9 +25,14 @@ var ManageAuthorPage = React.createClass({
       dirty: false
     };
   },
-  // componentWillMount: function () {
-  //
-  // },
+  componentWillMount: function () {
+    var authorId = this.props.params.id; // from the path '/author:id'
+    if (authorId) {
+      this.setState({
+        author: AuthorApi.getAuthorById(authorId)
+      });
+    }
+  },
   setAuthorState: function (event) {
     this.setState({ dirty: true });
     var field = event.target.name;
